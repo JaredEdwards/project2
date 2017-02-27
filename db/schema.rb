4ed36b89155e81cae825679af060138363acf7fa
jared edwards <jredwards519@gmail.com>
2017-02-27 17:49:02 -0500

    removed date:time column for redundancy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227221821) do
+ActiveRecord::Schema.define(version: 20170227224623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20170227221821) do
   create_table "ideas", force: :cascade do |t|
     t.string   "author"
     t.string   "title"
-    t.datetime "datetime"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Idea.create!({author: "Jared Edwards", title: "This sweet app", content: "Lorem IpsuM"})
